a7db271ff221899add9aec7fc3738909a26461ff
gatto <gatto@DESKTOP-C433F7S.home>
2022-03-29 08:58:57 +0200

    Versione 0.5

--- src/main/java/it/polito/tdp/corsi/FXMLController.java
+++ src/main/java/it/polito/tdp/corsi/FXMLController.java
@@ -5,7 +5,10 @@
 package it.polito.tdp.corsi;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.corsi.model.Corso;
 import it.polito.tdp.corsi.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -46,6 +49,28 @@ public class FXMLController {
 
     @FXML
     void corsiPerPeriodo(ActionEvent event) {
+    	txtRisultato.clear();
+    	String periodo = txtPeriodo.getText();
+    	int periodoNumerico;
+    	
+    	try {
+    		 periodoNumerico = Integer.parseInt(periodo);
+    	}catch(NumberFormatException e) {
+    		System.out.print("Inserire un periodo numerico");
+    		e.printStackTrace();
+    		return;
+    	}
+    	//se siamo qua vuol dire che abbiamo convertito successfully in numero
+    	if(periodoNumerico < 1 || periodoNumerico > 2) {
+    		txtRisultato.setText("Inserisci 1 o 2");
+    		return;
+    	}
+    	
+    	List<Corso> corsi = this.model.getCorsiByPeriodo(periodoNumerico);
+    	
+    	for(Corso c : corsi) {
+    		txtRisultato.appendText(c + "\n");
+    	}
     	
     }
 
